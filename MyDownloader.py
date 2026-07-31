95e860d7bdf19bbb3ac34b6266e436c03267565f
Joy Kumer Tarafder <joykumer698@gmail.com>
2026-07-31 16:25:31 +0600

    Update print statements

--- MyDownloader.py
+++ MyDownloader.py
@@ -794,18 +794,14 @@ def action_exit():
                    ##########                   
 """
     comment = r"""
-    ♥ THANK YOU FOR USING ♥
-    """
-    infos = r"""
-Git handle: Y2m777a5 | Git Repo: github.com/Y2m777a5/My-Downloader
-"""
-    cblock(f"{DEFAULT_COLOR}{logo}{WHITE}")
-    print()
-    cblock(f"{comment}")
-    print()
-    cblock(f"{DEFAULT_COLOR}{infos}")
-    print()
-    time.sleep(2.5)
+    ♥ THANK YOU FOR USING ♥"""
+    infos = r"""    Git handle: Y2m777a5 | Git Repo: github.com/Y2m777a5/My-DownloaderGit handle: Y2m777a5 | Git Repo: github.com/Y2m777a5/My-Downloader"""
+    cblock(f"{DEFAULT_COLOR}{logo}")
+    print(f"{WHITE}")
+    cline(f"  ♥ THANK YOU FOR USING ♥")
+    print(f"{DEFAULT_COLOR}")
+    cline(f"Git handle: Y2m777a5 | Git Repo: github.com/Y2m777a5/My-Downloader")
+    time.sleep(3)
     sys.exit()
 
 
